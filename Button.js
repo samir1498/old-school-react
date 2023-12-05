823721c02461a6118aa4d6c83938d77d769a3b36
samir <bettahar.samir@outlook.com>
2023-12-05 11:48:27 +0100

    generated files from babel

---
+++ Button.js
@@ -0,0 +1,3 @@
+export default function Button() {
+  return /*#__PURE__*/React.createElement("button", null, "Click me!");
+}
---
+++ MyApp.js
@@ -0,0 +1,9 @@
+import Button from "./Button.js";
+export default function MyApp() {
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    null,
+    /*#__PURE__*/ React.createElement("h1", null, "Welcome to my App"),
+    /*#__PURE__*/ React.createElement(Button, null)
+  );
+}
